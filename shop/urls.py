0fce1cd23eb58df0582cbb76ed69cd2884f97251
agorte077 <alexmaxninja077@gmail.com>
2023-08-11 23:43:53 +0200

    Authentification

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from shop.views import index, detail, add_to_cart, view_cart, remove_from_cart, login_view, signup_view
+from shop.views import index, detail,add_to_cart, view_cart, remove_from_cart, user_login,logOut,register,activate
 
 urlpatterns = [
     path('', index, name='Home'),
@@ -7,7 +7,8 @@ urlpatterns = [
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
     path('cart/', view_cart, name='cart'),
     path('remove_from_cart/<int:cart_item_id>/', remove_from_cart, name='remove_from_cart'),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('login/', login_view, name='login'),
-    path('signup/', signup_view, name='signup'),
+    path('register', register, name='register'),
+    path('login',user_login,name ='user_login'),
+    path('logout',logOut,name='logout'),
+    path('activate/<uidb64>/<token>',activate,name='activate')
 ]
